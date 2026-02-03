d028f1b53a7cfd33f2aaa64eedf56f6888853cc1
Rizky Alif Ramadhan <61588336+rizkyalifr@users.noreply.github.com>
2026-02-03 21:25:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ st.markdown("""
 TICKERS = ["PAXG-USD", "IDR=X"]
 MODAL_GAJI = 5000000 
 SPREAD_AJAIB = 1.015 
-PAXG_MULTIPLIER = 1.00326
+PAXG_MULTIPLIER = 0.99432278994
 
 # --- HELPER FORMATTING ---
 def fmt_idr(val): return f"Rp {val:,.0f}".replace(",", ".")
@@ -670,4 +670,5 @@ with col1:
 
 # Render Text Report in Code Block (Better CSS)
 st.code(final_report, language="yaml") # YAML syntax highlighting makes it look nice
+
 
